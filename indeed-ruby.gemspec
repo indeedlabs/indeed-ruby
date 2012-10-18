d41712562165a1dbf1e1b33bc2379f064e2a0597
Chad Masso <chad.m.masso@gmail.com>
2012-10-18 13:34:53 -0500

    initial commit

---
+++ indeed-ruby.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name = 'indeed-ruby'
+  s.version = '0.0.1'
+  s.author = 'Indeed Labs'
+  s.email = 'labs-team@indeed.com'
+
+  s.description = 'Indeed Job Search Ruby Api Client'
+  s.summary = 'Indeed Job Search Ruby Api Client'
+  s.homepage = 'http://github.com/indeedlabs/indeed-ruby'
+
+  s.files = ["lib/indeed-ruby.rb"]
+
+  s.add_dependency('rest-client', '>= 1.6.7')
+  s.add_dependency('json', '>= 1.7.5')
+end
---
+++ lib/indeed-ruby.rb
@@ -0,0 +1,66 @@
+require 'rest_client'
+require 'json'
+module Indeed
+    class Client
+
+        API_VERSION = '2'
+        API_ROOT = 'http://api.indeed.com/ads'
+        DEFAULT_FORMAT = 'json'
+
+        API_SEARCH = {
+            :end_point => "#{API_ROOT}/apisearch",
+            :required_fields => [:userip, :useragent, [:q, :l]],
+        }
+
+        API_JOBS = {
+            :end_point => "#{API_ROOT}/apigetjobs",
+            :required_fields => [:jobkeys],
+        }
+
+        def initialize(publisher)
+            @publisher = publisher
+        end
+
+        def search(params)
+            process_request(API_SEARCH[:end_point], valid_args(API_SEARCH[:required_fields], params))
+        end
+
+        def jobs(params)
+            valid_args = valid_args(API_JOBS[:required_fields], params)
+            valid_args[:jobkeys] = valid_args[:jobkeys].join(',')
+            process_request(API_JOBS[:end_point], valid_args)
+        end
+
+        private
+
+        def process_request(endpoint, args)
+            format = args.fetch(:format, DEFAULT_FORMAT)
+            raw = format == 'xml' ? true : args.fetch(:raw, false)
+            args.merge!({:v => API_VERSION, :publisher => @publisher, :format => format})
+            response = RestClient.get endpoint, {:params => args}
+            r = (not raw) ? JSON.parse(response.to_str) : response.to_str
+            r
+        end
+
+        def valid_args(required_fields, args)
+            for field in required_fields
+                if field.kind_of?(Array)
+                    has_one_required = false
+                    for f in field
+                        if args.has_key?(f)
+                            has_one_required = true
+                            break
+                        end
+                    end
+                    if not has_one_required
+                        raise ArgumentError.new('You must provide one of the following %s' % [field.join(', ')])
+                    end
+                elsif not args.has_key?(field)
+                    raise ArgumentError.new('The field %s is required' % [field])
+                end
+            end
+            args
+        end
+
+    end
+end
